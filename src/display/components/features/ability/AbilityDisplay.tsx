547650b92e48bb36042bb8fbb9400f259cda197b
Bob-The-Seagull-King <laneedwardsbrown@Gmail.com>
2025-02-14 21:40:52 +1300

    slight extra fixes

---
+++ src/display/components/features/ability/AbilityDisplay.tsx
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import '../../../../resources/styles/_mainstylesource.scss'
+import React from 'react'
+import { ErrorBoundary } from "react-error-boundary";
+
+// Classes
+import { GlossaryRule } from '../../../../classes/feature/glossary/Glossary'
+import { returnDescription } from '../../../../utility/util'
+import { ModelCollection } from '../../../../classes/feature/model/ModelCollection';
+import { Model } from '../../../../classes/feature/model/Model';
+import { Ability } from '../../../../classes/feature/ability/Ability';
+
+const AbilityDisplay = (props: any) => {
+    const abilityObject: Ability = props.data
+
+    return (
+        <ErrorBoundary fallback={<div>Something went wrong with AbilityDisplay.tsx</div>}>
+            <div className='abilityInternalStructure'>
+                <div>
+                </div>
+            </div>
+        </ErrorBoundary>
+    )
+}
+
+export default AbilityDisplay;
