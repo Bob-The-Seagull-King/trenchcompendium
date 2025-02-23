a1d83ef8d3dada3a6880286b9fe7e3d0a76cbb8f
Bob-The-Seagull-King <laneedwardsbrown@Gmail.com>
2025-02-24 12:47:38 +1300

    Test Data and TS interface setup

--- src/classes/feature/scenario/Scenario.ts
+++ src/classes/feature/scenario/Scenario.ts
@@ -3,6 +3,51 @@ import { ContextObject, IContextObject } from '../../contextevent/contextobject'
 import { StaticContextObject } from '../../contextevent/staticcontextobject';
 
 interface IScenario extends IContextObject {
+    description: [],
+    img_link : string,
+    forces : ForcesData,
+    battlefield : BattlefieldData,
+    infiltrators : InfiltratorData,
+    deployment : DeploymentData,
+    battle_length : BattleLengthData,
+    victory_conditions : VictoryData,
+    glorious_deeds : GloriousDeedData,
+    special_rules : string[]
+}
+
+interface ForcesData {
+    even_match: boolean,
+    description: []
+}
+
+interface BattlefieldData {
+    standard_terrain: boolean,
+    field_size: [],
+    extra_features: []
+}
+
+interface InfiltratorData {
+    even_match: number, // 0 = No, 1 = Yes, 2 = Special
+    description: []
+}
+
+interface DeploymentData {
+    description: []
+}
+
+interface VictoryData {
+    description: []
+}
+
+interface BattleLengthData {
+    min : number,
+    max : number,
+    description: []
+}
+
+interface GloriousDeedData {
+    deeds : string[],
+    optional_deeds : string[],
     description: []
 }
 
@@ -21,5 +66,5 @@ class Scenario extends StaticContextObject {
 
 }
 
-export {IScenario, Scenario}
+export {IScenario, Scenario, ForcesData, BattlefieldData, InfiltratorData, DeploymentData, VictoryData, BattleLengthData, GloriousDeedData}
 
