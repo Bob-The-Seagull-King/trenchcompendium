c2a5de20aa44500b9a22b3c5de9ac38d2a14ea34
Bob-The-Seagull-King <laneedwardsbrown@Gmail.com>
2025-03-05 21:06:53 +1300

    Pallete Rejig Test

--- src/utility/functions.ts
+++ src/utility/functions.ts
@@ -116,12 +116,6 @@ export function getColour(name: string){
         case "yellow": {
             return "yellow"
         }
-        case "faithful": {
-            return "yellow"
-        }
-        case "hell": {
-            return "red"
-        }
         case "red": {
             return "red"
         }
