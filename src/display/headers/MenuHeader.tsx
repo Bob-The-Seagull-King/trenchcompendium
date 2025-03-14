d2d2f39b665555375ae5faaa7ff1a198e7070b87
Bob-The-Seagull-King <laneedwardsbrown@Gmail.com>
2025-03-14 17:30:05 +1300

    Basic Setup

--- src/display/headers/MenuHeader.tsx
+++ src/display/headers/MenuHeader.tsx
@@ -1,29 +1,41 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../../resources/styles/_mainstylesource.scss'
-import React from 'react'
+import React, { useState } from 'react'
 import { ErrorBoundary } from "react-error-boundary";
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+// Font Aesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBurger, faGripLines, faHouse } from '@fortawesome/free-solid-svg-icons'
 
 // Components
 import PalleteSwap from './components/PalleteSwap';
 import LanguageSwap from './components/LanguageSwap';
+import { Button } from 'react-bootstrap';
 
-const MenuHeader = (prop: any) => {
+const MenuHeader = (prop: any) => {const [show, setShow] = useState(false);
 
-    // Return result -----------------------------
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
     return (
-        
+      
         <ErrorBoundary fallback={<div>Something went wrong with MenuHeader.tsx</div>}>
-            <div className="">
-                <div className={"floatingButton backgrounddefault"}>
-                    <PalleteSwap/>
-                </div>
-                <div className={"floatingButton backgrounddefault"}>
-                    <LanguageSwap/>
-                </div>
-            </div>
-            
+            <Button bsPrefix="empty" onClick={handleShow}>
+                <FontAwesomeIcon icon={faGripLines} className=" colourWhite"/>
+            </Button>
+    
+            <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Main Menu</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                Some text as placeholder. In real life you can have the elements you
+                have chosen. Like, text, images, lists, etc.
+            </Offcanvas.Body>
+            </Offcanvas>
         </ErrorBoundary>
-    )
+    );
     // -------------------------------------------
 }
 
