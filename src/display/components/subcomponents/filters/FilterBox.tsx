07a6813c4d5f5ce4d11f28f866bda31075451730
Bob-The-Seagull-King <laneedwardsbrown@Gmail.com>
2025-03-23 15:20:37 +1300

    Filter Box Shell

--- src/display/components/subcomponents/filters/FilterBox.tsx
+++ src/display/components/subcomponents/filters/FilterBox.tsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import '../../../../resources/styles/_mainstylesource.scss'
-import React from 'react'
+import React, { useState } from 'react'
 
 // Classes
 import { makestringpresentable } from '../../../../utility/functions'
@@ -8,8 +8,8 @@ import { ErrorBoundary } from "react-error-boundary";
 import { CollectionsListPage } from '../../../../classes/viewmodel/pages/CollectionListPage';
 import { FilterManager } from '../../../../classes/viewmodel/collections/filters/FilterManager';
 import { FilterText } from '../../../../classes/viewmodel/collections/filters/FilterInterfaces';
-import { InputGroup, Form, Button } from 'react-bootstrap';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { InputGroup, Form, Button, Collapse } from 'react-bootstrap';
+import { faFilter, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const FilterBox = (prop: any) => {
@@ -17,7 +17,8 @@ const FilterBox = (prop: any) => {
     const FilterManagerObj: FilterManager = ViewPageController.FilterManager;
     const updatesearch = prop.runfunction;
 
-    console.log()
+    
+    const [open, setOpen] = useState(false);
 
     function UpdateName( _filter : FilterText, newVal : string) {
         _filter.Val = newVal;
@@ -43,15 +44,64 @@ const FilterBox = (prop: any) => {
         )
     }
 
+    function ReturnTextParam(_filter : FilterText) {
+        return (            
+            <div className="backgroundBgBase">
+                <div className="colourBasicText size-subtitle">
+                    {makestringpresentable(_filter.Group)}
+                </div>
+                <Form.Control 
+                    onChange={e => UpdateName(_filter, e.target.value)} 
+                    className='bordergrey' 
+                    aria-label="Text input with checkbox" 
+                    defaultValue={_filter.Val}/>
+            </div>
+        )
+    }
+
+    function ReturnFilterBoxInner() {
+        return (
+            <div className="totalmarginsml">
+                <div>
+                    {FilterManagerObj.ReturnTextFilters().filter((item) => (item.Group != 'name')).map((item) =>
+                        <div key={item.Group}>
+                            {ReturnTextParam(item)}
+                        </div>)}
+                </div>
+            </div>
+        )
+    }
+
     // Return result -----------------------------
     return (
         <ErrorBoundary fallback={<div>Something went wrong with FilterBox.tsx</div>}>
-            <div className="">
-                {FilterManagerObj.ReturnTextFilters().filter((item) => (item.Group == 'name')).map((item) =>
-                <div key={item.Group}>
-                    {ReturnTextFilterParam(item)}
+            <div>
+                <div>
+                    {FilterManagerObj.ReturnTextFilters().filter((item) => (item.Group == 'name')).map((item) =>
+                    <div key={item.Group}>
+                        {ReturnTextFilterParam(item)}
+                    </div>)}
                 </div>
-                ) }
+                <div>
+                    <div className="verticalspacermed"/>
+                    <div className="borderthin bordergrey">
+                        <div onClick={() => {setOpen(!open)}} className={'centered-div size-strongtext font-default hovermouse colorBasicText centered-div borderbed bordergrey borderthin backgroundBgCard'}>
+                            <div className={'totalmarginxsm font-seriftext'}>
+                                {"Filters"}
+                            </div>
+                            <div className={'totalmarginxsm'}>
+                                <FontAwesomeIcon icon={faFilter} className="colorBasicText"/>
+                            </div>
+                        </div>
+                        <Collapse in={open}>
+                            <div className={'container borderthin bordergrey'}>
+                                <div className="content">                    
+                                    {ReturnFilterBoxInner()}
+                                </div>
+                            </div>
+                        </Collapse>    
+                    </div>                                    
+                </div>
             </div>
         </ErrorBoundary>
     )
