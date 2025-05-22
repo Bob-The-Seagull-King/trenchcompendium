c955ed704c7ac28898b839117bb0cc60d016b1c2
Lane Edwards-Brown <Lane.Edwards-Brown@skipton.co.uk>
2025-05-23 10:16:21 +1200

    whoopsies

--- src/display/components/features/faction/FactionCollectionDisplay.tsx
+++ src/display/components/features/faction/FactionCollectionDisplay.tsx
@@ -97,7 +97,7 @@ const FactionCollectionDisplay = (props: any) => {
                 <RulesBannerFaction
                     key={factioncollectionObject.GetBaseFac().ID}
                     slug={factioncollectionObject.GetBaseFac().ID}
-                    title={factioncollectionObject.GetBaseFac().Name}
+                    title={factioncollectionObject.GetBaseFac().Name? factioncollectionObject.GetBaseFac().Name : ""}
                 >
                     {(factioncollectionObject).SubModelsList.filter((item : any) => (item.var_name != 'base')).map(sub_item => (
                         <RulesBannerFaction
