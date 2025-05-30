fbd38c359179ea8b7f6f44bc2c2606e149c5acfc
Bob-The-Seagull-King <laneedwardsbrown@Gmail.com>
2025-05-30 14:06:19 +1200

    About Fin, Contact Formatted

--- src/display/pages/StaticAbout.tsx
+++ src/display/pages/StaticAbout.tsx
@@ -8,40 +8,27 @@ import imageroadmap from '../../resources/images/Roadmap.png'
 
 const StaticAbout: React.FC = () => {
     return (
-        <div className="StaticContact page-static">
+        <div className="StaticAbout page-static">
             <div className={'container'}>
                 <h1>About the Trench Companion</h1>
 
-                <div className={'RulesScenarioSummary rules-scenario-summary rules-card'}>
+                <div className={'rules-scenario-summary rules-card'}>
                     <div className={'rules-scenario-summary-title rules-card-title'}>
                         What Is This?
                     </div>
                                 
-                    <div className={'rules-scenario-summary-content rules-card-content'}>
+                    <div className={'rules-card-content'}>
                         <p>
-                            {"TEST TEXT"}
+                            {"The Trench-Companion is a 3rd-Party tool for players and lovers of Trench Crusade. This site aims to be the one-stop source for game information and the easiest place to create and maintain your own warbands, campaigns, and a place to access all manner of Trench Crusade content."}
                         </p>
                         <hr/>
                         <p>
-                            {"TEST TEXT"}
+                            {"Development on the Trench-Companion is continual and we plan to release updates and expand on the functionality present well into the future. Not only that, but we aim to maintain the site as the most up-to-date resource for Trench-Crusade available."}
                         </p>
-                        
-                        <table className={'rules-scenario-summary-table rules-card-table'}>
-                            <tbody>
-                                <tr>
-                                    <td className={'label-cell'}>
-                                        {"Question"}
-                                    </td>
-                                    <td>
-                                        {"Answer"}
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
                     </div>
                 </div>
 
-                <div className={'RulesScenarioSummary rules-scenario-summary rules-card'}>
+                <div className={'rules-scenario-summary rules-card'}>
                     <div className={'rules-scenario-summary-title rules-card-title'}>
                         Future Plans
                     </div>
@@ -58,18 +45,34 @@ const StaticAbout: React.FC = () => {
                             <tbody>
                                 <tr>
                                     <td className={'label-cell'}>
-                                        {"Update"}
+                                        {"Genesis Update (July 2025)"}
                                     </td>
                                     <td>
-                                        {"desc"}
+                                        {"In this initial release of Trench-Companion a full rules compendium will maintain an up-to-date catalogue of all game rules. Not only that, but the Warband Buidler will allow users to create and maintain warbands effortlessly."}
                                     </td>
                                 </tr>
                                 <tr>
                                     <td className={'label-cell'}>
-                                        {"Update2"}
+                                        {"It Never Ends Update (September 2025)"}
                                     </td>
                                     <td>
-                                        {"desc2"}
+                                        {"In the first major update to Trench-Companion the Campaign Manager and Play Mode will be added. A campaign manager allows users to associate their warbands with a Campaign, tracking progress across games. Not only that, but scenarios can be played out in the Play Mode to track victory points, glorious deeds, and all other manner of game features."}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className={'label-cell'}>
+                                        {"Pride Cometh Update (January 2026)"}
+                                    </td>
+                                    <td>
+                                        {"In the second major update the ability for Homebrew Content to be created will be massively expanded. Expansive documentation and guides will be created to help users create their own custom homebrew content and add it to their games. Premium users will also receive a glut of new options and the ability to have their homebrew content hosted for other users to access."}
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td className={'label-cell'}>
+                                        {"??? Update (???)"}
+                                    </td>
+                                    <td>
+                                        {"Who knows what the future holds for the Trench Companion."}
                                     </td>
                                 </tr>
                             </tbody>
--- src/display/pages/StaticContact.tsx
+++ src/display/pages/StaticContact.tsx
@@ -11,63 +11,74 @@ const StaticContact: React.FC = () => {
             <div className={'container'}>
                 <h1>Contact</h1>
 
-                <div className={'RulesScenarioSummary rules-scenario-summary rules-card'}>
-                    <div className={'rules-scenario-summary-title rules-card-title'}>
-                        Support
-                    </div>
-                                
-                    <div className={'rules-scenario-summary-content rules-card-content'}>
-                        <table className={'rules-scenario-summary-table rules-card-table'}>
-                            <tbody>
-                                <tr>
-                                    <td className={'label-cell'}>
-                                        {"Email"}
-                                    </td>
-                                    <td>
-                                        {"Answer"}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className={'label-cell'}>
-                                        {"Something Else?"}
-                                    </td>
-                                    <td>
-                                        {"Answer2"}
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                <div className={'row'}>
 
-                <div className={'RulesScenarioSummary rules-scenario-summary rules-card'}>
-                    <div className={'rules-scenario-summary-title rules-card-title'}>
-                        Business Inquiries
+                    <div className={'rules-scenario-summary rules-card col-12 col-sm-6 col-xs-6'}>
+                        <div className={'rules-scenario-summary-title rules-card-title'}>
+                            Support
+                        </div>
+                                    
+                        <div className={'rules-scenario-summary-content rules-card-content'}>
+                            <table className={'rules-scenario-summary-table rules-card-table'}>
+                                <tbody>
+                                    <tr>
+                                        <td className={'label-cell'}>
+                                            {"Email"}
+                                        </td>
+                                        <td>
+                                            {"Answer"}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={'label-cell'}>
+                                            {"Phone"}
+                                        </td>
+                                        <td>
+                                            {"Answer2"}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <div className={"ContactFormSupport"}>
+
+                            </div>
+                        </div>
                     </div>
-                                
-                    <div className={'rules-scenario-summary-content rules-card-content'}>
-                        <table className={'rules-scenario-summary-table rules-card-table'}>
-                            <tbody>
-                                <tr>
-                                    <td className={'label-cell'}>
-                                        {"Email"}
-                                    </td>
-                                    <td>
-                                        {"Answer"}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td className={'label-cell'}>
-                                        {"Something Else?"}
-                                    </td>
-                                    <td>
-                                        {"Answer2"}
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+
+                    <div className={'RulesScenarioSummary rules-scenario-summary rules-card col-12 col-sm-6 col-xs-6'}>
+                        <div className={'rules-scenario-summary-title rules-card-title'}>
+                            Business Inquiries
+                        </div>
+                                    
+                        <div className={'rules-scenario-summary-content rules-card-content'}>
+                            <table className={'rules-scenario-summary-table rules-card-table'}>
+                                <tbody>
+                                    <tr>
+                                        <td className={'label-cell'}>
+                                            {"Email"}
+                                        </td>
+                                        <td>
+                                            {"Answer"}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={'label-cell'}>
+                                            {"Phone"}
+                                        </td>
+                                        <td>
+                                            {"Answer2"}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                                <div className={"ContactFormBusiness"}>
+
+                                </div>
+                            </table>
+                        </div>
                     </div>
+
                 </div>
+                
             </div>
         </div>
     )
