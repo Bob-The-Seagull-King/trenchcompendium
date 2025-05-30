e6306f71a75b3c67b01d1cf4bc86c449f48a0a98
David Leusch <info@momentum-digital.de>
2025-05-30 11:53:58 +0200

    fixed pricate user data API url

--- src/factories/synod/UserFactory.ts
+++ src/factories/synod/UserFactory.ts
@@ -118,7 +118,7 @@ class UserFactory {
             // /* @TODO David please add/show how to get the proper stuff to make this request work */
             // const response : Response = await fetch(`${SYNOD.URL}/wp-json/synod/v1/user-private/${_val}`)
 
-            const response = await fetch(`${SYNOD.URL}/wp-json/synod/v1/user-private/${_val}`, {
+            const response = await fetch(`${SYNOD.URL}/wp-json/synod/v1/user-full/${_val}`, {
                 headers: {
                     'Authorization': `Bearer ${token}`,
                 },
