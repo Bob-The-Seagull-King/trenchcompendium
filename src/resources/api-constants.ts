819b01ac41de2ffc1e46bcc0f30233f87c195d67
David Leusch <info@momentum-digital.de>
2025-06-27 13:36:38 +0200

    Checked Sandbox Client ID

--- src/resources/api-constants.ts
+++ src/resources/api-constants.ts
@@ -14,7 +14,11 @@ export const SYNOD = {
 
     PP_PLAN_MONTH_ID: 'P-1RH12116WR818940FNAVTBDI', // The Plan-ID for the monthly paypal subscription
     PP_PLAN_YEAR_ID: 'P-60U62498HE309964JNAVTCBI', // The Plan-ID for the yearly paypal subscription
+
     PP_CLIENT_ID: 'AREbTEkX_IfbYs5fjuz54_aCppkKrZi_lWfZXIGWt4DSw_gwF9zfQ40gLB8SeUU5yNrehIgeeIG5uuED', // The API base for Paypal subscription services
+    PP_CLIENT_ID_SANDBOX: 'AREbTEkX_IfbYs5fjuz54_aCppkKrZi_lWfZXIGWt4DSw_gwF9zfQ40gLB8SeUU5yNrehIgeeIG5uuED', // For reference
+    PP_CLIENT_ID_LIVE: '', // For reference
+
     PP_API_BASE: 'https://api-m.sandbox.paypal.com', // The API base for Paypal subscription services
     PP_API_SANDBOX: 'https://api-m.sandbox.paypal.com', // for reference
     PP_API_LIVE: 'https://api.paypal.com', // for reference
