c605357d99e3ce5bbb8587df2179211591a76408
David Leusch <info@momentum-digital.de>
2025-06-27 13:48:29 +0200

    updated Paypal sandbox credentials

--- src/resources/api-constants.ts
+++ src/resources/api-constants.ts
@@ -12,8 +12,13 @@ export const SYNOD = {
     DEV: 'http://synod.trench-companion.test', // for reference
     URL: 'https://synod.trench-companion.com', // The currently used API url
 
-    PP_PLAN_MONTH_ID: 'P-1RH12116WR818940FNAVTBDI', // The Plan-ID for the monthly paypal subscription
-    PP_PLAN_YEAR_ID: 'P-60U62498HE309964JNAVTCBI', // The Plan-ID for the yearly paypal subscription
+    PP_PLAN_MONTH_ID: 'P-4YK72240DG152144TNBN7EKI', // The Plan-ID for the monthly paypal subscription
+    PP_PLAN_MONTH_ID_SANDBOX: 'P-4YK72240DG152144TNBN7EKI', // Reference
+    PP_PLAN_MONTH_ID_LIVE: 'P-1RH12116WR818940FNAVTBDI', // Reference
+
+    PP_PLAN_YEAR_ID: 'P-784241401A144594JNBPII7Q', // The Plan-ID for the yearly paypal subscription
+    PP_PLAN_YEAR_ID_SANDBOX: 'P-784241401A144594JNBPII7Q', // Reference
+    PP_PLAN_YEAR_ID_LIVE: 'P-60U62498HE309964JNAVTCBI', // Reference
 
     PP_CLIENT_ID: 'AREbTEkX_IfbYs5fjuz54_aCppkKrZi_lWfZXIGWt4DSw_gwF9zfQ40gLB8SeUU5yNrehIgeeIG5uuED', // The API base for Paypal subscription services
     PP_CLIENT_ID_SANDBOX: 'AREbTEkX_IfbYs5fjuz54_aCppkKrZi_lWfZXIGWt4DSw_gwF9zfQ40gLB8SeUU5yNrehIgeeIG5uuED', // For reference
