92cbd4430b714038de4eb4810f4dabd7906c6c3e
Bob-The-Seagull-King <laneedwardsbrown@Gmail.com>
2025-07-19 13:46:15 +1200

    FactionModel Relationship

--- src/resources/staticcontext/BaseContextTable.tsx
+++ src/resources/staticcontext/BaseContextTable.tsx
@@ -1164,10 +1164,15 @@ export const BaseContextCallTable : CallEventTable = {
                 }
             }
             if (context_func["warband_limit"]) {
+                const CurThresh = await trackVal.GetCampaignTresholdValue()
+                let CurSize = relayVar;
                 for (let i = 0; i < context_func["warband_limit"].length; i++) {
-                    /** @TODO When we figure out campaign attatchment and warband limit */
-                    return relayVar;
+                    if (context_func["warband_limit"][i]["size"] <= CurThresh) {
+                        CurSize = context_func["warband_limit"][i]["value"]
+                    }
+                    
                 }
+                return CurSize;
             }
             
             return relayVar;
