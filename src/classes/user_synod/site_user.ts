91ec506cc0d54afd88d37d0b72e3fe3f03a12040
David Leusch <info@momentum-digital.de>
2025-07-24 10:46:01 +0200

    Fixed date formatting for JS Timestamp

--- src/classes/user_synod/site_user.ts
+++ src/classes/user_synod/site_user.ts
@@ -450,7 +450,7 @@ class SiteUser {
     }
 
     public PremiumUntilFormat () {
-        const date = new Date(this.PremiumUntil());
+        const date = new Date(this.PremiumUntil() * 1000);
 
         const day = String(date.getDate()).padStart(2, '0');
         const month = String(date.getMonth() + 1).padStart(2, '0');
