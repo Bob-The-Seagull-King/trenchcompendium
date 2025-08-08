8dd8c4401ef06ee4633f7b3d4b3e771aa2e71d48
David Leusch <info@momentum-digital.de>
2025-08-08 10:09:53 +0200

    Added display for multiplayer games history

--- src/display/campaign-manager/micro-components/CMHistoryGame.tsx
+++ src/display/campaign-manager/micro-components/CMHistoryGame.tsx
@@ -1,6 +1,8 @@
 import {useCampaign} from "../../../context/CampaignContext";
 import SynodImageWithCredit from "../../../utility/SynodImageWithCredits";
 import React from "react";
+import {useNavigate} from "react-router-dom";
+import CustomNavLink from "../../components/subcomponents/interactables/CustomNavLink";
 
 
 interface CMHistoryGameProps {
@@ -27,46 +29,65 @@ interface CMHistoryGameProps {
  */
 const CMHistoryGame: React.FC<CMHistoryGameProps> = (game) => {
     const { campaign } = useCampaign();
+    const navigate = useNavigate();
 
     return (
         <div className="CMHistoryGame">
-
-
             <div className={'CMHistoryGame-Game'}>
-                <div className={'CMHistoryGame-Game-main'}>
-                    {game.game.warbands.map((warband, index) => (
-                        <div className={'CMHistoryGame-Game-warband warband-count-2'} key={index}>
-                            <SynodImageWithCredit
-                                imageId={warband.warbandImageId}
-                                className={'CMHistoryGame-Game-warband-image'}
-                            />
+                <div className={'CMHistoryGame-Game-main'}> {/* @TODO: make this a link to the game */}
+                    {Array.from({ length: Math.ceil(game.game.warbands.length / 2) }, (_, i) => {
+                        const pair = game.game.warbands.slice(i * 2, i * 2 + 2);
+                        const isIncompleteRow = pair.length < 2;
 
-                            <div className={'CMHistoryGame-Game-text'}>
-                                <div className={'CMHistoryGame-Game-warband-name'}>
-                                    {warband.warbandName}
-                                </div>
-                                <div className={'CMHistoryGame-Game-player-name'}>
-                                    {warband.playerName}
-                                </div>
+                        return (
+                            <div className={`CMHistoryGame-Game-row ${isIncompleteRow ? "incomplete-row" : ""}`} key={i}>
+                                {game.game.warbands.slice(i * 2, i * 2 + 2).map((warband, index) => (
+                                    <div className={`CMHistoryGame-Game-warband ${index % 2 === 0 ? "left" : "right"}`}
+                                         key={index}
+                                    >
+                                        <SynodImageWithCredit
+                                            imageId={warband.warbandImageId}
+                                            className={'CMHistoryGame-Game-warband-image'}
+                                        />
+
+                                        <div className={'CMHistoryGame-Game-text'}>
+                                            <div className={'CMHistoryGame-Game-warband-name'}>
+                                                {warband.warbandName}
+                                            </div>
+                                            <div className={'CMHistoryGame-Game-player-name'}>
+                                                {warband.playerName}
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))}
                             </div>
-
-                        </div>
-                    ))}
+                        );
+                    })}
                 </div>
 
                 <div className={'CMHistoryGame-Players'}>
                 {game.game.warbands.map((warband, index) => (
                         <div className={'CMHistoryPlayer'} key={index}>
-                            <div className={'player-image-wrap'}>
+                            <CustomNavLink
+                                classes={'player-image-wrap'}
+                                link={`/profile/${warband.playerId}`}
+                                runfunc={() => {
+                                    navigate(`/profile/${warband.playerId}`, {state: Date.now().toString()})
+                                }}>
                                 <SynodImageWithCredit
                                     imageId={warband.playerImageId}
                                     className={''}
                                 />
-                            </div>
+                            </CustomNavLink>
 
-                            <div className={'CMHistoryPlayer-name'}>
+                            <CustomNavLink
+                                classes={'CMHistoryPlayer-name'}
+                                link={`/profile/${warband.playerId}`}
+                                runfunc={() => {
+                                    navigate(`/profile/${warband.playerId}`, {state: Date.now().toString()})
+                                }}>
                                 {warband.playerName}
-                            </div>
+                            </CustomNavLink>
                         </div>
                     ))}
                 </div>
