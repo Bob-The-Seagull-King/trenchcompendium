e39985f96b019887ab1aa265af1d25167741de09
David Leusch <info@momentum-digital.de>
2025-08-14 18:13:22 +0200

    Added Campaign Invites on profile page

--- src/display/components/Profile/ProfilePageCampaigns.tsx
+++ src/display/components/Profile/ProfilePageCampaigns.tsx
@@ -7,6 +7,7 @@ import UserListEntry from "./UserListEntry";
 import CampaignListEntry from "./CampaignListEntry";
 import {SiteUser} from "../../../classes/user_synod/site_user";
 import {SiteUserPublic} from "../../../classes/user_synod/user_public";
+import ProfilePageCampaignsInvite from './ProfilePageCampaignsInvite';
 
 interface ProfilePageCampaignsProps {
     userData: SiteUser | SiteUserPublic | null;
@@ -42,6 +43,17 @@ const ProfilePageCampaigns: React.FC<ProfilePageCampaignsProps> = ({ userData })
 
     ]
 
+    const dummyPlayers = [
+        { profilePictureId: 1001, playerName: 'Player One' },
+        { profilePictureId: 1002, playerName: 'Player Two' },
+        { profilePictureId: 1003, playerName: 'Player Three' },
+        { profilePictureId: 1004, playerName: 'Player Four' },
+        { profilePictureId: 1005, playerName: 'Player Five' },
+        { profilePictureId: 1006, playerName: 'Player Six' },
+        { profilePictureId: 1007, playerName: 'Player Seven' },
+        { profilePictureId: 1008, playerName: 'Player Eight' },
+    ];
+
     return (
         <div className="ProfilePageCampaigns">
             <div className={'profile-card'}>
@@ -71,6 +83,15 @@ const ProfilePageCampaigns: React.FC<ProfilePageCampaignsProps> = ({ userData })
                             {'This feature is still in developement, stay tuned for the next update.'}
                         </div>
                     )}
+
+                    {/* @TODO: Show if user has campaign invites*/}
+                    { dummyPlayers.length > 0 &&
+                        <ProfilePageCampaignsInvite
+                            campaignName="Test Campaign"
+                            campaignId={164}
+                            players={dummyPlayers}
+                        />
+                    }
 
                 </div>
             </div>
