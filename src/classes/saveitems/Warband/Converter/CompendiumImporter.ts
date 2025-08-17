8badf159f34ccc2b0bf8486c10f6118b9aa8201c
Bob-The-Seagull-King <laneedwardsbrown@Gmail.com>
2025-08-17 14:45:04 +1200

    member equipment

--- src/classes/saveitems/Warband/Converter/CompendiumImporter.ts
+++ src/classes/saveitems/Warband/Converter/CompendiumImporter.ts
@@ -76,15 +76,6 @@ class CompendiumImporter {
         NewWarband.warband_data.Debts.ducats += JSONVal.DucatLost;
         NewWarband.warband_data.Debts.glory += JSONVal.GloryLost;
 
-        // Models
-        const ModelOptionList = await (NewWarband.warband_data).GetFighterOptions(false);
-        const ModelFullList = await ModelFactory.GetAllFactionModel([], []);
-        for (let i = 0; i < JSONVal.Members.length; i++) {
-            const mod_id = ConvertCompendiumToCompanionID(JSONVal.Members[i].Model.ID);
-            const item_costtype = JSONVal.Members[i].Model.CostType == "ducats"? 0 : 1;
-            await this.BuildModel(mod_id, NewWarband.warband_data, JSONVal.Members[i].Model.Cost, item_costtype, ModelOptionList, ModelFullList, JSONVal.Members[i]);
-        }
-        
         // Stash
         const OptionList = await (NewWarband.warband_data).GetFactionEquipmentOptions(true, false, true, false);
         const FullList = await EquipmentFactory.GetAllFactionEquipment(true, [], []);
@@ -93,6 +84,16 @@ class CompendiumImporter {
             const item_costtype = JSONVal.Armoury[i].CostType == "ducats"? 0 : 1;
             await this.BuildEquipment(mod_id, NewWarband.warband_data, JSONVal.Armoury[i].Cost, item_costtype, OptionList, FullList);
         }
+
+        // Models
+        const ModelOptionList = await (NewWarband.warband_data).GetFighterOptions(false);
+        const ModelFullList = await ModelFactory.GetAllFactionModel([], []);
+        for (let i = 0; i < JSONVal.Members.length; i++) {
+            const mod_id = ConvertCompendiumToCompanionID(JSONVal.Members[i].Model.ID);
+            const item_costtype = JSONVal.Members[i].Model.CostType == "ducats"? 0 : 1;
+            await this.BuildModel(mod_id, NewWarband.warband_data, JSONVal.Members[i].Model.Cost, item_costtype, ModelOptionList, ModelFullList, JSONVal.Members[i], OptionList, FullList);
+        }
+        
 
         // Locations
         for (let i = 0; i < JSONVal.Locations.length; i++) {
@@ -153,7 +154,11 @@ class CompendiumImporter {
         }
     }
 
-    public async BuildModel(item_id : string, wb : UserWarband, cost_val : number, cost_type : number, options : FactionModelRelationship[], fulloptions : FactionModelRelationship[], data : any) {
+    public async BuildModel(
+        item_id : string, wb : UserWarband, cost_val : number, cost_type : number, 
+        options : FactionModelRelationship[], fulloptions : FactionModelRelationship[], 
+        data : any,
+        refoptions : FactionEquipmentRelationship[], reffulloptions : FactionEquipmentRelationship[]) {
         const op_id = options.map(obj => obj.Model.ID)
         try {
             
@@ -179,7 +184,7 @@ class CompendiumImporter {
                 if (cost_val > 0) {FacRel.Cost = cost_val}
                 FacRel.CostType = cost_type;
                 const Added : WarbandPurchase[] = await wb.AddFighter([FacRel], isfree);
-                for (let j = 0; j < Added.length; j++) { await this.BuildModelFull(Added[j], wb, data);}
+                for (let j = 0; j < Added.length; j++) { await this.BuildModelFull(Added[j], wb, data, refoptions, reffulloptions);}
             }
 
         } catch (e) {
@@ -188,7 +193,8 @@ class CompendiumImporter {
         }
     }
 
-    public async BuildModelFull(mdl : WarbandPurchase, wb : UserWarband, data : any) {
+    public async BuildModelFull(mdl : WarbandPurchase, wb : UserWarband, data : any,
+        refoptions : FactionEquipmentRelationship[], reffulloptions : FactionEquipmentRelationship[]) {
         const GetModel = mdl.HeldObject as WarbandMember;
 
         GetModel.Name = data.Name;
@@ -197,6 +203,51 @@ class CompendiumImporter {
         GetModel.SaveNote(data.Notes, "notes")
         if (data.Reserve == true) {
             GetModel.State = "reserved";
+        }
+
+        // Equipment
+        for (let i = 0; i < data.Equipment.length; i++) {
+            const mod_id = ConvertCompendiumToCompanionID(data.Equipment[i].ID);
+            const item_costtype = data.Equipment[i].CostType == "ducats"? 0 : 1;
+            await this.BuildModelEquipment(mod_id, GetModel, data.Equipment[i].Cost, item_costtype, refoptions, reffulloptions);
+        }
+
+        // Skills
+
+        // Injuries
+
+        // Upgrades
+    }
+
+    public async BuildModelEquipment(item_id : string, md : WarbandMember, cost_val : number, cost_type : number, options : FactionEquipmentRelationship[], fulloptions : FactionEquipmentRelationship[]) {
+        const op_id = options.map(obj => obj.EquipmentItem.ID)
+        try {
+            let FacRel : FactionEquipmentRelationship | null = null;
+            if (op_id.includes(item_id)) {
+                for (let i = 0; i < options.length; i++) {
+                    if (options[i].EquipmentItem.ID == item_id) {
+                        FacRel = options[i]
+                        break;
+                    }
+                }
+            } else {
+                for (let i = 0; i < fulloptions.length; i++) {
+                    if (fulloptions[i].EquipmentItem.ID == item_id) {
+                        FacRel = fulloptions[i]                        
+                        break;
+                    }
+                }
+            }
+
+            if (FacRel != null) {
+                FacRel.Cost = cost_val;
+                FacRel.CostType = cost_type;
+                await md.AddEquipment(FacRel);
+            }
+
+        } catch (e) {
+            console.log(e)
+            console.log(item_id)
         }
     }
 
