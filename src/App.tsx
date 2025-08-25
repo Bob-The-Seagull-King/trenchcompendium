72dc94be349326af85dde31860b81d7424717de9
David Leusch <info@momentum-digital.de>
2025-08-25 17:17:51 +0200

    removed maintenance notice

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,7 @@ const App: React.FC = () => {
 
                                 <ScrollToTop/>
 
-                                <MaintananceNotice />
+                                {/*<MaintananceNotice />*/}
 
                                 <Routes>
                                     <Route path={ROUTES.HOME_ROUTE} element={<HomeRoute />} />
