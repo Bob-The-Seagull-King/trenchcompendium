069c0909c5447b3a3f60bb19ec6a23ee6033b65c
David Leusch <info@momentum-digital.de>
2025-09-10 12:17:44 +0200

    Improved fighter card stats to show +xD for melee and ranged

--- src/display/components/rules-content/FighterCard/FighterCardStats.tsx
+++ src/display/components/rules-content/FighterCard/FighterCardStats.tsx
@@ -1,5 +1,7 @@
 // FighterCardStats.tsx
 import React from 'react';
+import {getModelStatMove} from "../../../../classes/feature/model/ModelStats";
+import ItemStat from "../../subcomponents/description/ItemStat";
 
 interface FighterCardStatsProps {
     movement: string;
@@ -11,22 +13,22 @@ interface FighterCardStatsProps {
 const FighterCardStats: React.FC<FighterCardStatsProps> = ({ movement, ranged, melee, armour }) => {
     return (
         <div className="fighter-card-stats">
-            <div className="fighter-card-stat">
-                <div className="stat-label">Movement</div>
-                <div className="stat-value">{movement}</div>
-            </div>
-            <div className="fighter-card-stat">
-                <div className="stat-label">Melee</div>
-                <div className="stat-value">{melee}</div>
-            </div>
-            <div className="fighter-card-stat">
-                <div className="stat-label">Ranged</div>
-                <div className="stat-value">{ranged}</div>
-            </div>
-            <div className="fighter-card-stat">
-                <div className="stat-label">Armour</div>
-                <div className="stat-value">{armour}</div>
-            </div>
+            <ItemStat
+                title={"Movement"}
+                value={movement}
+            />
+            <ItemStat
+                title={"Melee"}
+                value={melee}
+            />
+            <ItemStat
+                title={"Ranged"}
+                value={ranged}
+            />
+            <ItemStat
+                title={"Armour"}
+                value={armour}
+            />
         </div>
     );
 };
