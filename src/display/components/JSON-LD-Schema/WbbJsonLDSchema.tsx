b1916e5517e5c463b6cd7ff91128f126df523ae6
David Leusch <info@momentum-digital.de>
2025-10-17 10:42:17 +0200

    Improved Warband Schema Markup

--- src/display/components/JSON-LD-Schema/WbbJsonLDSchema.tsx
+++ src/display/components/JSON-LD-Schema/WbbJsonLDSchema.tsx
@@ -1,5 +1,5 @@
 // WbbJsonLDSchema.tsx
-import React, { useEffect, useMemo } from "react";
+import React, {useEffect, useMemo, useRef} from "react";
 import { useWarband } from "../../../context/WarbandContext";
 import { RealWarbandPurchaseModel } from "../../../classes/saveitems/Warband/Purchases/WarbandPurchase";
 
@@ -11,15 +11,27 @@ function safeJsonLdStringify(obj: unknown) {
         .replace(/&/g, "\\u0026");
 }
 
+
+/**
+ * This creates JSON LD markup for the warband on the first render.
+ * - @TODO: now only references to members and items are shown. These members should be added later.
+ *
+ */
 const WbbJsonLDSchema: React.FC = () => {
 
     const {warband, updateKey, reloadDisplay} = useWarband();
+
+    // holds the very first JSON string we generated (frozen)
+    const frozenJsonRef = useRef<string | null>(null);
 
     if ( !warband ) {return null;}
 
     const UserWarband = warband.warband_data;
 
+    // URL of the page
     const base_url = typeof window !== "undefined" ? window.location.origin : "https://trench-companion.com";
+
+    // URL to this warband
     const wb_url = base_url + '/warband/detail/' + warband.id;
 
     /** Build the faction URL*/
@@ -52,6 +64,30 @@ const WbbJsonLDSchema: React.FC = () => {
         return `/${clean}/`;
     }
 
+    // list of warband Members for Warband Schema
+    const wb_members = UserWarband.GetFighters()
+        // optional: nur aktive Member nehmen
+        // .filter(m => !m.isDead)
+        .map((m : RealWarbandPurchaseModel, i) => ({
+            "@type": "ListItem",
+            position: i + 1,
+            item: {
+                "@id": `${wb_url}#${m.model.ID}`
+            }
+        }));
+
+    // List of Locations for Warband Schema
+    const wb_locations = UserWarband.GetLocations()
+        .map((loc, i) => ({
+            "@type": "ListItem",
+            position: i + 1,
+            item: {
+                "@id": `${wb_url}#${loc.ID}`
+            }
+    }));
+
+    // @TODO: Add list of modifiers to warband schema
+
     const wb_data = {
         '@context'      : 'https://schema.org',
         '@type'         : 'WebPage',
@@ -66,15 +102,73 @@ const WbbJsonLDSchema: React.FC = () => {
             'name'          : UserWarband.GetFactionName(),
             'url'           : faction_URL,
         },
-        'description'   : UserWarband.GetWarbandNotes(),
-        'text'          : UserWarband.GetLore(),
+        ...(UserWarband.GetWarbandNotes() && { description: UserWarband.GetWarbandNotes() }),
+        ...(UserWarband.GetLore() && { text: UserWarband.GetLore() }),
+        "additionalProperty": [
+            { "@type": "PropertyValue", "name": "Rating (Glory)",
+                "value": UserWarband.GetCostDucats(),   "unitText": "Glory"
+            },
+            { "@type": "PropertyValue", "name": "Rating (Ducats)",
+                "value": UserWarband.GetCostGlory(),  "unitText": "Ducats"
+            },
+            { "@type": "PropertyValue", "name": "Value (Ducats)",
+                "value": UserWarband.GetCostDucatsTotal(), "unitText": "Ducats"
+            },
+            { "@type": "PropertyValue", "name": "Value (Glory)",
+                "value": UserWarband.GetCostGloryTotal(),   "unitText": "Glory"
+            },
+            { "@type": "PropertyValue", "name": "Victory Points",
+                "value": UserWarband.GetVictoryPoints()
+            },
+            { "@type": "PropertyValue", "name": "Failed Promotions",
+                "value": UserWarband.Context.FailedPromotions
+            },
+            { "@type": "PropertyValue", "name": "Campaign Round",
+                "value": UserWarband.GetCampaignCycleView()
+            },
+            { "@type": "PropertyValue", "name": "Campaign Notes",
+                "value": UserWarband.GetCampaignNotes()
+            }
+        ],
+        "hasPart": [
+            {
+                "@type": "ItemList",
+                "name": "Members",
+                "itemListElement": wb_members
+            },
+            {
+                "@type": "ItemList",
+                "name": "Exploration Locations",
+                "itemListElement": wb_locations
+            },
+        ]
     }
+
+    /**
+     * For each of the following create JSON LD Schema which is linked by the id in the warband Schema:
+     * - Fighters
+     * - Locations
+     * - Modifiers
+     */
+
+
+    // compute current JSON string
+    const currentJson = safeJsonLdStringify(wb_data);
+
+    // on first pass, freeze it; after that, never change again
+    useEffect(() => {
+        if (frozenJsonRef.current === null) {
+            frozenJsonRef.current = currentJson;
+        }
+    }, [currentJson]);
+
+    // prefer the frozen first snapshot; fallback to current for the initial mount
+    const jsonToRender = frozenJsonRef.current ?? currentJson;
 
     return (
         <script
             type="application/ld+json"
-            // Kein pretty print in Prod; safeJson gegen </script>-Breakout
-            dangerouslySetInnerHTML={{ __html: safeJsonLdStringify(wb_data) }}
+            dangerouslySetInnerHTML={{ __html: jsonToRender }}
         />
     );
 }
