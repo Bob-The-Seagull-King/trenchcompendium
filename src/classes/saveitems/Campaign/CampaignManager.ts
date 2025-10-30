237b7c2ac929bb4c9f77cb8607579ea86f18aa8a
David Leusch <info@momentum-digital.de>
2025-10-30 12:20:20 +0100

    Same word, another typo lol

--- src/classes/saveitems/Campaign/CampaignManager.ts
+++ src/classes/saveitems/Campaign/CampaignManager.ts
@@ -258,7 +258,7 @@ class CampaignManager {
         }
     }
 
-    public async MoveWarandInviteCampaign(_val : number, _accepted : boolean) {
+    public async MoveWarbandInviteCampaign(_val : number, _accepted : boolean) {
         let NewCampaign : Campaign | null = null;
         for (let i = 0; i < this.ListOfWarbandInvites.length; i++) {
             if (this.ListOfWarbandInvites[i].GetId() == _val) {
